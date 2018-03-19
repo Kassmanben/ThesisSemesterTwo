f7608e22191c2ed7ff15e42cbe4f032faf0e97bd
Kassmanben <Kassmanben@gmail.com>
2018-03-18 19:59:13 -0700

    Updated

--- pageProcessor.py
+++ pageProcessor.py
@@ -1,6 +1,7 @@
 import shutil
 
 import numpy as np
+import re
 import scipy.misc
 import pickle
 from PIL import Image
@@ -9,12 +10,13 @@ import cv2
 import os
 import io
 import PyPDF2
+import difflib
+
 import wand
 import wand.image
 from wand.color import Color
 
-
-filename = 'Algorithms.pdf'
+filename = 'Sedgewick_ALGORITHMS_ED4_3513.pdf'
 base = os.path.basename(filename)
 directory_name = os.path.splitext(base)[0]
 
@@ -30,6 +32,8 @@ ocr_page_text = {}
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 correct_text_by_page = {}
+
+final_text = {}
 
 
 def mark_state(test_img, page_num):
@@ -62,7 +66,7 @@ def mark_state(test_img, page_num):
 
 
 def process_page(page_num):
-    pageObj = pdfReader.getPage(page_num-1)
+    pageObj = pdfReader.getPage(page_num - 1)
     page_text = pageObj.extractText()
     page_text = page_text.replace("\n", " ")
     correct_text_by_page[page_num] = page_text
@@ -88,14 +92,32 @@ def process_page(page_num):
         except KeyError:
             ocr_page_text[page_num] = text
 
-min_pg = 1
-max_pg = 10
 
-for pg_num in range(min_pg, max_pg):
-    process_page(pg_num)
+def fill_final_text(page_number, ocr_text, pdf_text):
+    str = ""
+    error = {"ocr": [], "pdf": []}
 
-for pg_num in range(min_pg, max_pg):
-    print(ocr_page_text[pg_num])
-    print(correct_text_by_page[pg_num]+"\n\n\n")
+    ocr_text = ocr_text.split(".")
+    pdf_text = pdf_text.split(".")
 
-shutil.rmtree(directory_name+" Parts")
+    diff = difflib.unified_diff(ocr_text, pdf_text)
+    for e in list(diff):
+        if(e.startswith("+")):
+            print(e)
+
+
+min_pg = 12
+max_pg = 14
+
+pg_num = 12
+
+# for pg_num in range(min_pg, max_pg):
+#     print(pg_num)
+process_page(pg_num)
+
+# for pg_num in range(min_pg, max_pg):
+ocr_page_text[pg_num] = clean_up_string(ocr_page_text[pg_num])
+correct_text_by_page[pg_num] = clean_up_string(correct_text_by_page[pg_num])
+fill_final_text(pg_num, ocr_page_text[pg_num], correct_text_by_page[pg_num])
+
+shutil.rmtree(directory_name + " Parts")
